7e85d01fdb71662eaf69c7a5244fff5760554a07
Ben Ng <neong83@hotmail.com>
2021-10-17 10:39:08 -0700

    add CORS, JSON post body support, and test routes to express service

--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -1,9 +1,28 @@
 import express from "express";
+import { router } from "./api/testRoutes";
 
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
+// CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  next();
+});
+
+// Parse JSON bodies for this app. Make sure you put
+// `app.use(express.json())` **before** your route handlers!
+app.use(express.json());
+
+
+app.use("/api/v1", router);
+
+app.get("/", (req, res, next) => {
   res.send("Hello world");
 });
 
